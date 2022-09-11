0d13d4e91bcc293216fdf657bcbb5c0bbe08c8b2
azoozalharte <zezo50115@protonmail.com>
2022-09-12 00:10:20 +0300

    hide the word, view letter if guessed true

--- hangman.js
+++ hangman.js
@@ -1,9 +1,26 @@
-const Hangman = function (word, number) {
-  this.word = word;
+const Hangman = function (word, number, guessedLetters) {
+  this.word = word.toLowerCase().split("");
   this.number = number;
+  this.guessedLetters = guessedLetters;
+};
+// car
+// ***
+// azo alharte
+// *** *******
+Hangman.prototype.getPuzzle = function () {
+  let guessedLetterArr = this.guessedLetters.toLowerCase().split("");
+  let puzzle = "";
+  this.word.map((val) => {
+    if (guessedLetterArr.includes(val)) {
+      puzzle += val;
+    } else if (val != " ") {
+      puzzle += "*";
+    } else {
+      puzzle += val;
+    }
+  });
+  return puzzle;
 };
 
-const word1 = new Hangman("cat", 3);
-console.log(word1);
-const word2 = new Hangman("dog", 3);
-console.log(word2);
+const word1 = new Hangman("red car", 3, "rcv");
+console.log(word1.getPuzzle());
