1fcaaff58d7c1f3126726017666434847e07c8d6
azoozalharte <zezo50115@protonmail.com>
2022-09-15 22:46:24 +0300

    make guess

--- hangman.js
+++ hangman.js
@@ -19,5 +19,16 @@ Hangman.prototype.getPuzzle = function () {
   return puzzle;
 };
 
+Hangman.prototype.makeGuess = function (letter) {
+  letter = letter.toLowerCase();
+  if (!this.guessedLetters.includes(letter)) {
+    if (!this.word.includes(letter)) {
+      this.number--;
+    }
+    this.guessedLetters.push(letter);
+    this.getPuzzle();
+  }
+};
+
 const word1 = new Hangman("abdulaziz", 3);
 console.log(word1.getPuzzle());
