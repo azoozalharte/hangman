b5012e876f31dda259ba2f4db3ad83e0f00dad2c
azoozalharte <zezo50115@protonmail.com>
2022-09-16 01:38:26 +0300

    new style

--- hangman.js
+++ hangman.js
@@ -23,7 +23,14 @@ Hangman.prototype.makeGuess = function (letter) {
   letter = letter.toLowerCase();
   if (!this.guessedLetters.includes(letter)) {
     if (!this.word.includes(letter)) {
+      const uncorrectLetter = document.createElement("p");
+      uncorrectLetter.textContent = letter;
+      document.querySelector(".uncorrect-letters").appendChild(uncorrectLetter);
       this.numberOfGuesses--;
+    } else {
+      const correctLetter = document.createElement("p");
+      correctLetter.textContent = letter;
+      document.querySelector(".correct-letters").appendChild(correctLetter);
     }
     this.guessedLetters.push(letter);
     this.getPuzzle();
