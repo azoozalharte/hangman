499c424674aeb4c1203060b9b7bc8632d18302b9
azoozalharte <zezo50115@protonmail.com>
2022-09-16 19:23:31 +0300

    using class

--- hangman.js
+++ hangman.js
@@ -1,64 +1,64 @@
 ("use strict");
-const Hangman = function (word, numberOfGuesses) {
-  this.word = word.toLowerCase().split("");
-  this.numberOfGuesses = numberOfGuesses;
-  this.guessedLetters = [];
-  this.status = "playing";
-};
-
-Hangman.prototype.playingStatus = function () {
-  if (this.numberOfGuesses === 0) {
-    return (this.status = "failed");
-  } else if (this.word.join("") === this.getPuzzle()) {
-    return (this.status = "finished");
-  } else {
-    return this.status;
+class Hangman {
+  constructor(word, numberOfGuesses) {
+    this.word = word.toLowerCase().split("");
+    this.numberOfGuesses = numberOfGuesses;
+    this.guessedLetters = [];
+    this.status = "playing";
   }
-};
-
-Hangman.prototype.statusMessage = function () {
-  if (this.status === "failed") {
-    return `خسرت التحدي الكلمة هي  "${this.word.join("")}"`;
-  } else if (this.status === "finished") {
-    return "أهنيك أكتشفت الكلمة";
-  } else {
-    return "";
-  }
-};
-
-Hangman.prototype.getPuzzle = function () {
-  let puzzle = "";
-  this.word.map((val) => {
-    if (this.guessedLetters.includes(val)) {
-      puzzle += val;
-    } else if (val != " ") {
-      puzzle += "*";
+  playingStatus() {
+    if (this.numberOfGuesses === 0) {
+      return (this.status = "failed");
+    } else if (this.word.join("") === this.getPuzzle()) {
+      return (this.status = "finished");
     } else {
-      puzzle += val;
+      return this.status;
     }
-  });
-  return puzzle;
-};
-
-Hangman.prototype.makeGuess = function (letter) {
-  letter = letter.toLowerCase();
-  if (this.status != "playing") {
-    return;
   }
-  if (!this.guessedLetters.includes(letter)) {
-    if (!this.word.includes(letter)) {
-      const uncorrectLetter = document.createElement("p");
-      uncorrectLetter.textContent = letter;
-      document.querySelector(".uncorrect-letters").appendChild(uncorrectLetter);
-      this.numberOfGuesses--;
+  statusMessage() {
+    if (this.status === "failed") {
+      return `خسرت التحدي الكلمة هي  "${this.word.join("")}"`;
+    } else if (this.status === "finished") {
+      return "أهنيك أكتشفت الكلمة";
     } else {
-      const correctLetter = document.createElement("p");
-      correctLetter.textContent = letter;
-      document.querySelector(".correct-letters").appendChild(correctLetter);
+      return "";
     }
-    this.guessedLetters.push(letter);
-    this.getPuzzle();
   }
+  getPuzzle() {
+    let puzzle = "";
+    this.word.map((val) => {
+      if (this.guessedLetters.includes(val)) {
+        puzzle += val;
+      } else if (val != " ") {
+        puzzle += "*";
+      } else {
+        puzzle += val;
+      }
+    });
+    return puzzle;
+  }
+  makeGuess(letter) {
+    letter = letter.toLowerCase();
+    if (this.status != "playing") {
+      return;
+    }
+    if (!this.guessedLetters.includes(letter)) {
+      if (!this.word.includes(letter)) {
+        const uncorrectLetter = document.createElement("p");
+        uncorrectLetter.textContent = letter;
+        document
+          .querySelector(".uncorrect-letters")
+          .appendChild(uncorrectLetter);
+        this.numberOfGuesses--;
+      } else {
+        const correctLetter = document.createElement("p");
+        correctLetter.textContent = letter;
+        document.querySelector(".correct-letters").appendChild(correctLetter);
+      }
+      this.guessedLetters.push(letter);
+      this.getPuzzle();
+    }
 
-  this.playingStatus();
-};
+    this.playingStatus();
+  }
+}
